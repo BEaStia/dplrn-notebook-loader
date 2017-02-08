7bf9a85b6632779b6ab97c6317046b20e4cfbb29
Igor Pavlov <gophan1992@gmail.com>
2017-02-09 02:24:20 +0300

    initial #2

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 import os
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(
     name="dplrn-notebook-loader",
     version='0.0.1',
